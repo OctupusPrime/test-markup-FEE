aee0dd48f2cc06175109d4471a21bb8a670601e0
Octupus Prime <rasiapypkin2014@gmail.com>
2021-08-26 00:06:27 +0300

    improved code quality

--- js/main.js
+++ js/main.js
@@ -1,44 +1,56 @@
+//Slider
 const $sliderCont = document.querySelector('.slider-container'),
       $sliderRow = document.querySelector('.slider-row'),
       $slides = document.querySelectorAll('.slider-row .slide'),
       $dotsRow = document.querySelector('.section-slider .dots-row')
       slideTarget = 150,
-      slideOffset = 100
+      sliderPaddings = 100,
+      slideGap = 100
 
-let slideWidth = $sliderCont.clientWidth - 100,
+let slideWidth = $sliderCont.clientWidth - sliderPaddings,
     sliderStart = false,
     startPos = 0,
     secondPos = 0,
     slideIndex = 0,
     dotsItems = ''
 
-for (let slide of $slides) {
-    slide.style.minWidth = slideWidth + 'px'
+//Count dots items
+for (let i = 0; i < $slides.length; i++) {
     dotsItems += '<div class="dot-item"></div>'
 }
-
 $dotsRow.innerHTML = dotsItems
 const $dotsItems = document.querySelectorAll('.section-slider .dots-row .dot-item')
 $dotsItems[slideIndex].classList.add('active')
 
-const movement = (val) => {//changeIndex
+const calcSlideWidth = (width) => {
+    $slides.forEach(slide => {
+        slide.style.width = width + 'px'
+    })
+}
+
+const changeSlideIndex = (val) => {
     $dotsItems[slideIndex].classList.remove('active')
-    if (val)
+    if (val) {
         slideIndex++
-    else
+    }
+    else {
         slideIndex--
-    if (slideIndex > ($slides.length - 1))
-        slideIndex = 0;
-    if (slideIndex < 0)
+    }
+
+    if (slideIndex > ($slides.length - 1)) {
+        slideIndex = 0
+    }
+    if (slideIndex < 0) {
         slideIndex = ($slides.length - 1)
+    }
 
     $dotsItems[slideIndex].classList.add('active')
 
-    sliderController(slideIndex)
+    sliderControllerTransform(slideIndex)
 } 
 
-const sliderController = (slide) => {
-    const distance = slide * (slideWidth + slideOffset)
+const sliderControllerTransform = (slide) => {
+    const distance = slide * (slideWidth + slideGap)
 
     $sliderRow.style.cssText = `transform: translateX(${-1 * (distance)}px)`
 }
@@ -53,7 +65,7 @@ const move = (e) => {
     if (sliderStart) {
         secondPos = - e.pageX + startPos
 
-        $sliderRow.style.cssText = `transform: translateX(${-1 * (secondPos + slideIndex * (slideWidth + slideOffset))}px)`
+        $sliderRow.style.cssText = `transform: translateX(${-1 * (secondPos + slideIndex * (slideWidth + slideGap))}px)`
     }
 }
 
@@ -63,37 +75,19 @@ const end = () => {
 
         sliderStart = false
 
-        if (secondPos > slideTarget && slideIndex < ($slides.length - 1))
-            movement(true)
-        else if (secondPos < -slideTarget && slideIndex > 0)
-            movement(false)
-
-        sliderController(slideIndex)
+        if (secondPos > slideTarget && slideIndex < ($slides.length - 1)) {
+            changeSlideIndex(true)
+        }
+        else if (secondPos < -slideTarget && slideIndex > 0) {
+            changeSlideIndex(false)
+        }
+   
+        sliderControllerTransform(slideIndex)
         secondPos = 0
     }
 }
 
-document.querySelector('.section-slider').addEventListener('click', (e) => {//buttons
-    const btn = e.target.closest('button')
-    if (btn) {
-        if (btn.dataset.sliderMove === 'left')
-            movement(false)
-        else if (btn.dataset.sliderMove === 'right')
-            movement(true)
-    }
-})
-
-window.addEventListener('resize', () => {//resized
-    $sliderRow.classList.remove('animate')
-
-    slideWidth = $sliderCont.clientWidth - 100 
-
-    for (let slide of $slides) {
-        slide.style.minWidth = slideWidth + 'px'
-    }
-
-    sliderController(slideIndex)
-})
+calcSlideWidth(slideWidth)
 
 $sliderCont.addEventListener('mousedown', (e) => {//mouse
     start(e)
@@ -116,6 +110,31 @@ window.addEventListener('touchend', () => {
     end()
 })
 
+window.addEventListener('resize', () => {//resized
+    $sliderRow.classList.remove('animate')
+
+    slideWidth = $sliderCont.clientWidth - sliderPaddings 
+
+    calcSlideWidth(slideWidth)
+
+    sliderControllerTransform(slideIndex)
+})
+
+//SliderButtons
+document.querySelector('.section-slider').addEventListener('click', (e) => {
+    const btn = e.target.closest('button')
+    if (btn) {
+        if (btn.dataset.sliderMove === 'left') {
+            changeSlideIndex(false)
+        }
+        else if (btn.dataset.sliderMove === 'right') {
+            changeSlideIndex(true)
+        }
+    }
+})
+
+
+//MobileMenu
 document.getElementById('menu-btn').addEventListener('click', (e) => {
     document.querySelector('.mobile-nav').classList.toggle('show')
 })
